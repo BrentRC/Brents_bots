4818f9814cc9d75eff0873aa92bdd55eec839683
BrentRC <112354813+BrentRC@users.noreply.github.com>
2024-01-20 13:18:14 -0500

    added set gravity

--- simulate.py
+++ simulate.py
@@ -2,6 +2,7 @@ import pybullet as p
 import time
 physicsClient = p.connect(p.GUI)
 # p.configureDebugVisualizer(p.COV_ENABLE_GUI,0)
+p.setGravity(0,0,-9.8)
 p.loadSDF("box.sdf")
 for i in range(1000):
     p.stepSimulation()
