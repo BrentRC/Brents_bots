04f72679c1a76bb30924b28651642261a42a8851
BrentRC <112354813+BrentRC@users.noreply.github.com>
2024-01-20 13:50:13 -0500

    added a loop to make a tower of increasingly smaller blocks

--- generate.py
+++ generate.py
@@ -9,6 +9,12 @@ z = 0.5
 x2 = 1
 y2 = 0
 z2 = 1.5
-pyrosim.Send_Cube(name="Box", pos=[x,y,z] , size=[length,width,height])
-pyrosim.Send_Cube(name="Box2", pos=[x2,y2,z2] , size=[length,width,height])
+#pyrosim.Send_Cube(name="Box", pos=[x,y,z] , size=[length,width,height])
+#pyrosim.Send_Cube(name="Box2", pos=[x2,y2,z2] , size=[length,width,height])
+for i in range(10):
+    pyrosim.Send_Cube(name="Box", pos=[x, y, z], size=[length, width, height])
+    z = z + 1
+    length = length * 0.9
+    width = width * 0.9
+    height = height * 0.9
 pyrosim.End()
