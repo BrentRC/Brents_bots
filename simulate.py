33fe3230a1edc9a8b49165e825e47c11179d863c
BrentRC <112354813+BrentRC@users.noreply.github.com>
2024-02-10 12:19:08 -0500

    finished storing sensors now i need to save sensor values

--- simulate.py
+++ simulate.py
@@ -1,6 +1,8 @@
 import pybullet_data
 import pybullet as p
 import time
+import pyrosim.pyrosim as pyrosim
+import numpy
 physicsClient = p.connect(p.GUI)
 p.setAdditionalSearchPath(pybullet_data.getDataPath())
 # p.configureDebugVisualizer(p.COV_ENABLE_GUI,0)
@@ -8,7 +10,11 @@ p.setGravity(0,0,-9.8)
 planeId = p.loadURDF("plane.urdf")
 robotId = p.loadURDF("body.urdf")
 p.loadSDF("world.sdf")
+pyrosim.Prepare_To_Simulate(robotId)
+backLegSensorValues = numpy.zeros(1000)
 for i in range(1000):
     p.stepSimulation()
+    backLegSensorValues[i] = pyrosim.Get_Touch_Sensor_Value_For_Link("BackLeg")
     time.sleep(1/60)
 p.disconnect()
+print(backLegSensorValues)
