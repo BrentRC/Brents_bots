8c74f1be985e7e95f7b203d54644df180ae7f91b
BrentRC <112354813+BrentRC@users.noreply.github.com>
2024-02-19 12:31:07 -0500

    made sinusoidal vector and graph for motion

--- simulate.py
+++ simulate.py
@@ -1,8 +1,10 @@
+import math
+import random
 import pybullet_data
 import pybullet as p
 import time
 import pyrosim.pyrosim as pyrosim
-import numpy
+import numpy as np
 physicsClient = p.connect(p.GUI)
 p.setAdditionalSearchPath(pybullet_data.getDataPath())
 # p.configureDebugVisualizer(p.COV_ENABLE_GUI,0)
@@ -11,9 +13,14 @@ planeId = p.loadURDF("plane.urdf")
 robotId = p.loadURDF("body.urdf")
 p.loadSDF("world.sdf")
 pyrosim.Prepare_To_Simulate(robotId)
-backLegSensorValues = numpy.zeros(10000)
-frontLegSensorValues = numpy.zeros(10000)
-for i in range(10000):
+backLegSensorValues = np.zeros(1000)
+frontLegSensorValues = np.zeros(1000)
+
+lin_space = np.linspace(0, 2 * np.pi, 1000)
+sin_vec = np.sin(lin_space)
+
+
+for i in range(1000):
     p.stepSimulation()
     backLegSensorValues[i] = pyrosim.Get_Touch_Sensor_Value_For_Link("BackLeg")
     frontLegSensorValues[i] = pyrosim.Get_Touch_Sensor_Value_For_Link("FrontLeg")
@@ -21,10 +28,17 @@ for i in range(10000):
         bodyIndex = robotId,
         jointName = b'Torso_BackLeg',
         controlMode = p.POSITION_CONTROL,
-        targetPosition = 0.0,
-        maxForce = 500)
+        targetPosition = sin_vec[i],
+        maxForce = 50)
+    pyrosim.Set_Motor_For_Joint(
+        bodyIndex=robotId,
+        jointName=b'Torso_FrontLeg',
+        controlMode=p.POSITION_CONTROL,
+        targetPosition= sin_vec[i],
+        maxForce=50)
     time.sleep(1/60)
 p.disconnect()
-numpy.save('data/BackLegSensorValues.npy', backLegSensorValues)
-numpy.save('data/FrontLegSensorValues.npy', frontLegSensorValues)
+np.save('data/BackLegSensorValues.npy', backLegSensorValues)
+np.save('data/FrontLegSensorValues.npy', frontLegSensorValues)
+np.save('data/sin_vecValues.npy', sin_vec)
 print(backLegSensorValues)
